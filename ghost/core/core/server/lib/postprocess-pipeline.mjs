944ef11f6dcb92407e50a6af64dff3ff2ef1b485
Isham Faizal <ishamf@users.noreply.github.com>
2024-06-07 23:41:37 +0800

    feat: change to not recreate the processor

--- ghost/core/core/server/lib/postprocess-pipeline.mjs
+++ ghost/core/core/server/lib/postprocess-pipeline.mjs
@@ -1,21 +1,22 @@
 import { rehype } from 'rehype'
 import rehypeShiki from '@shikijs/rehype'
 
+const processor = rehype()
+    .data('settings', { fragment: true })
+    .use(rehypeShiki, {
+        themes: {
+            light: 'github-light',
+            dark: 'dark-plus',
+        }
+    })
+
 /**
  *
  * @param {string} html
  * @returns
  */
 export async function pipeline(html) {
-    const file = await rehype()
-        .data('settings', { fragment: true })
-        .use(rehypeShiki, {
-            themes: {
-                light: 'github-light',
-                dark: 'dark-plus',
-            }
-        })
-        .process(html)
+    const file = await processor.process(html)
 
     return String(file)
 }
